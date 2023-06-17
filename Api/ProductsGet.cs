a740a2dcb7015bdfa1004139aac872fad10a872f
ideal.light <ideal.light@outlook.com>
2023-06-16 23:27:47 -0700

    Get products

---
+++ Api/ProductsGet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Api;
+
+public class ProductsGet
+{
+   readonly IProductData productData;
+   public ProductsGet(IProductData productData)
+    {
+        this.productData = productData;
+    }
+
+    [FunctionName("ProductsGet")]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null )]
+        HttpRequest req,
+        ILogger log)
+    {
+        var products = await productData.GetProducts();
+
+        return new OkObjectResult(products);
+    }
+}
+
